b4d6dc60f350ef38e189d9081cc77559ff48bff3
David Boström <david.bostrom@medieinstitutet.se>
2023-10-12 20:24:24 +0200

    Add edit, save, delete contact functions

--- script.js
+++ script.js
@@ -17,10 +17,42 @@ function createContact(event) {
 	`
 		<input type="text" value="${createContactName.value}" disabled>
 		<input type="text" value="${createContactPhone.value}" disabled>
-		<input type="button" value="Ändra">
-		<input type="button" value="Radera">
+		<input type="button" class="editContactButton" value="Ändra">
+		<input type="button" class="saveContactButton" value="Spara" hidden>
+		<input type="button" class="deleteContactButton" value="Radera">
 	`;
+	contactListItem.getElementsByClassName('editContactButton')[0].addEventListener('click', editContact);
+	contactListItem.getElementsByClassName('saveContactButton')[0].addEventListener('click', saveContact);
+	contactListItem.getElementsByClassName('deleteContactButton')[0].addEventListener('click', deleteContact);
+
 	contactList.appendChild(contactListItem);
+}
+
+function editContact(event) {
+	const targetListItem = event.target.parentNode;
+	console.log(`Edit mode enabled on contact ${targetListItem.children[0].value}`);
+
+	targetListItem.children[0].disabled = false;
+	targetListItem.children[1].disabled = false;
+	targetListItem.children[2].hidden = true;
+	targetListItem.children[3].hidden = false;
+}
+
+function saveContact(event) {
+	const targetListItem = event.target.parentNode;
+
+	targetListItem.children[0].disabled = true;
+	targetListItem.children[1].disabled = true;
+	targetListItem.children[2].hidden = false;
+	targetListItem.children[3].hidden = true;
+
+	console.log(`Saved contact ${targetListItem.children[0].value}`);
+}
+
+function deleteContact(event) {
+	const targetListItem = event.target.parentNode;
+	targetListItem.remove();
+	console.log(`Contact ${targetListItem.children[0].value} deleted`);
 }
 
 createContactButton.addEventListener('click', createContact);
