030cc01748865742f8f5981184476e810a1398c6
David Boström <david.bostrom@medieinstitutet.se>
2023-10-13 18:14:45 +0200

    Add input validation

--- script.js
+++ script.js
@@ -3,8 +3,37 @@
 const createContactButton = document.getElementById('createContactButton');
 const clearListButton = document.getElementById('clearListButton');
 
+function validateInput(event) {
+	const targetParent = event.target.parentNode;
+	const textFields = targetParent.querySelectorAll('input[type="text"], input[type="tel"]');
+
+	// Remove any previous error messages
+	if (targetParent.querySelector('.errorMessage')) {
+		targetParent.querySelector('.errorMessage').remove();
+	}
+	
+	// Check for empty text fields
+	for (const textField of textFields) {
+		if (!textField.value) {
+			console.log(`Text field '${textField.id}' / '${textField.className}' is empty`);
+
+			let errorMessage = document.createElement('p');
+			errorMessage.className = 'errorMessage';
+			errorMessage.innerText = 'Kontakter får inte innehålla tomma fält';
+			targetParent.appendChild(errorMessage);
+
+			return false;
+		}
+	}
+}
+
 function createContact(event) {
 	event.preventDefault();
+
+	// Validate text fields, cancel if any are empty
+	if (validateInput(event) === false) {
+		return;
+	}
 
 	const createContactName = document.getElementById('createContactName');
 	const createContactPhone = document.getElementById('createContactPhone');
@@ -13,8 +42,8 @@ function createContact(event) {
 	const listItem = document.createElement('li');
 	listItem.innerHTML =
 	`
-		<input type="text" value="${createContactName.value}" disabled>
-		<input type="text" value="${createContactPhone.value}" disabled>
+		<input type="text" class="contactName" value="${createContactName.value}" disabled>
+		<input type="tel" class="contactPhone" value="${createContactPhone.value}" disabled>
 		<input type="button" class="editContactButton" value="Ändra">
 		<input type="button" class="saveContactButton" value="Spara" hidden>
 		<input type="button" class="deleteContactButton" value="Radera">
@@ -22,7 +51,6 @@ function createContact(event) {
 	listItem.getElementsByClassName('editContactButton')[0].addEventListener('click', toggleEditContact);
 	listItem.getElementsByClassName('saveContactButton')[0].addEventListener('click', toggleEditContact);
 	listItem.getElementsByClassName('deleteContactButton')[0].addEventListener('click', deleteContact);
-
 	contactList.appendChild(listItem);
 
 	console.log(`Created contact with name '${createContactName.value}' and phone number '${createContactPhone.value}'`);
@@ -31,7 +59,12 @@ function createContact(event) {
 function toggleEditContact(event) {
 	const targetListItem = event.target.parentNode;
 
-	// Toggle booleans:
+	// Validate text fields, cancel if any are empty
+	if (validateInput(event) === false) {
+		return;
+	}
+
+	// Toggle booleans
 	targetListItem.children[0].disabled = !targetListItem.children[0].disabled;
 	targetListItem.children[1].disabled = !targetListItem.children[1].disabled;
 	targetListItem.children[2].hidden = !targetListItem.children[2].hidden;
@@ -45,9 +78,7 @@ function toggleEditContact(event) {
 
 function deleteContact(event) {
 	const targetListItem = event.target.parentNode;
-	
 	targetListItem.remove();
-
 	console.log(`Deleted contact '${targetListItem.children[0].value}'`);
 }
 
@@ -55,12 +86,10 @@ function clearList() {
 	const listItems = document.querySelectorAll('#contactList li');
 
 	console.log(`Deleting all contacts...`);
-
 	for (const listItem of listItems) {
 		listItem.remove();
 		console.log(`Deleted contact '${listItem.children[0].value}'`);
 	}
-
 	console.log(`All contacts deleted`);
 }
 
